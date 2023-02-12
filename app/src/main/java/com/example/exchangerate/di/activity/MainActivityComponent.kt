8531d4048f12faed19ee72d6b81e17d20a80ff4d
ErroredPasta <ForeignServices3132@pm.me>
2023-02-12 23:57:54 +0900

    Subcomponent의 modules의 값으로 MainActivityModule을 지정하고 Factory에서 생성시 activity를 parameter로 넘겨주도록 수정

--- app/src/main/java/com/example/exchangerate/di/activity/MainActivityComponent.kt
+++ app/src/main/java/com/example/exchangerate/di/activity/MainActivityComponent.kt
@@ -1,14 +1,15 @@
 package com.example.exchangerate.di.activity
 
 import com.example.exchangerate.presentation.MainActivity
+import dagger.BindsInstance
 import dagger.Subcomponent
 
-@Subcomponent
+@Subcomponent(modules = [MainActivityModule::class])
 interface MainActivityComponent {
 
     @Subcomponent.Factory
     interface Factory {
-        fun create(): MainActivityComponent
+        fun create(@BindsInstance activity: MainActivity): MainActivityComponent
     }
 
     fun inject(activity: MainActivity)
